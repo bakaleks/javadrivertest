6c32111a54537da5020e5833567d37354fe90400
alebakan <alexei.bakanov@finn.no>
2013-10-04 21:48:30 +0200

    added files

---
+++ src/main/java/javadrivertest/App.java
@@ -0,0 +1,51 @@
+package javadrivertest;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ConsistencyLevel;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int loglines = 0;
+
+        Cluster cluster = Cluster.builder().addContactPoint("localhost").build();
+        Session session = cluster.connect("testks");
+
+        long epoch = System.currentTimeMillis();
+        for (int sequence = 1; sequence <= 1000; sequence++) {
+            session.execute(QueryBuilder
+                                .insertInto("table1")
+                                .values(new String[]{"epoch", "sequence", "bogus"},
+                                        new Object[]{epoch, sequence, "bogusvalue"})
+                                .setConsistencyLevel(ConsistencyLevel.QUORUM));
+        }
+        System.out.println(String.format("%d: Rows written", loglines++));
+
+        while (true) {
+            int exceptionCounter = 0;
+            for (int sequence = 1; sequence <= 1000; sequence++) {
+                try {
+                    session.execute("SELECT * FROM table1 WHERE epoch = " + epoch + " AND sequence = " + sequence + ";");
+                    session.execute(QueryBuilder
+                                   .select().all().from("table1")
+                            .where(QueryBuilder.eq("epoch", epoch)).and(QueryBuilder.eq("sequence", sequence))
+                            .setConsistencyLevel(ConsistencyLevel.QUORUM));
+                } catch (Exception e) {
+                    exceptionCounter++;
+                    System.out.println(e.getMessage());
+                }
+            }
+
+            System.out.printf("%d: Tried to fetch 1000 rows and got %d exceptions%n", loglines++, exceptionCounter);
+        }
+
+
+    }
+}
